e368ecd53f301735e616b81bef0db0a1e995bee5
sol92824 <sol92824@gmail.com>
2025-10-19 19:39:47 +0900

    chatgpt-clone Jupyter Notebook 도식화  + 응답 포맷 설정

--- main.py
+++ main.py
@@ -3,21 +3,36 @@ import asyncio
 
 dotenv.load_dotenv()
 
-from agents import Agent, Runner, SQLiteSession
+from agents import Agent, Runner, SQLiteSession, function_tool
+from agents.extensions.visualization import draw_graph
+from pydantic import BaseModel
 
 # session_id : 세션 식별 구분자
 # db path : 세션 저장소 위치 (기본값 : ":memory:" - 일회성 세션)
 session = SQLiteSession("user_1", "ai-memory.db")
 
+class Answer(BaseModel):
+    answer: str
+    background_explanation: str
+
+@function_tool
+def get_weather():
+    return "30"
+
+
 geography_agent = Agent(
     name = "Geo Expert Agent",
     instructions = "당신은 지리 전문가로서, 지리에 관한 모든 질문에 전문적으로 답변합니다.",
     # handoff 발생 조건 설명
-    handoff_description = "이 Agent를 사용하여 지리와 관련된 질문에 답변하세요."
+    handoff_description = "이 Agent를 사용하여 지리와 관련된 질문에 답변하세요.",
+    tools = [
+        get_weather
+    ],
+    output_type = Answer
 )
 
 economics_agent = Agent(
-    name = "Economics  Expert Agent",
+    name = "Economics Expert Agent",
     instructions = "당신은 경제 전문가로서, 경제에 관한 모든 질문에 전문적으로 답변합니다.",
     # handoff 발생 조건 설명
     handoff_description = "이 Agent를 사용하여 경제와 관련된 질문에 답변하세요."
@@ -34,13 +49,19 @@ main_agent = Agent(
 )
 
 async def main():
+    # agent 구동에 대한 도식화
+    # 연결된 agent / agent에서 호출 가능한 tools 등
+    # 그냥 .py 파일에서는 실행해도 새로운 파일이 생기거나 도식화 되지 않음
+    # 찾아보니 Jupyter Notebook용 시각화 함수인 듯?
+    # draw_graph(main_agent)
+
     # run : 최종 처리 내역 리턴
     # run_sync : await를 사용하지 못하거나 사용하지 않고 싶을 때 (run과 결과값 동일)
     # run_streamed : 실시간으로 처리 내역 리턴
     result = await Runner.run(
         # starting agent
         main_agent,
-        "국가들이 채권을 파는 이유가 뭐야?",
+        "태국 북부 지방의 수도는 어디야?",
         session = session,
     )
 

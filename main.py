e2d7fa692f35200170b1b3a3518c520f9277b9d0
sol92824 <sol92824@gmail.com>
2025-10-19 22:23:43 +0900

    chatgpt-clone chat ui 업그레이드 (대화 히스토리 노출 + 답변 실시간 노출)

--- main.py
+++ main.py
@@ -19,23 +19,6 @@ if "agent" not in st.session_state:
 
 agent = st.session_state["agent"]
 
-async def run_agent(message):
-    stream = Runner.run_streamed(
-        agent, 
-        message,
-        session = session
-    )
-
-    async for event in stream.stream_events():
-        if event.type == "raw_response_event":
-            if event.data.type == "response.output_text.delta":
-                with st.chat_message("ai"):
-                    st.write(event.data.delta)
-
-############################################
-#################### UI ####################
-############################################
-
 # 세션 : 최초 1번만 초기화
 if "session" not in st.session_state:
     st.session_state["session"] = SQLiteSession(
@@ -45,10 +28,46 @@ if "session" not in st.session_state:
 
 session = st.session_state["session"]
 
+# 이전 대화 내용 보여주기
+async def paint_history():
+    messages = await session.get_items()
+
+    for message in messages:
+        with st.chat_message(message["role"]):
+            if message["role"] == "user":
+                st.write(message["content"])
+            else:
+                if message["type"] == "message":
+                    st.write(message["content"][0]["text"])
+
+asyncio.run(paint_history())
+
+# 채팅으로 전달받은 내용 agent로 전달 + 응답값 화면 노출
+async def run_agent(message):
+    with st.chat_message("assistant"):
+        text_placeholder = st.empty()
+        response = ""
+
+        stream = Runner.run_streamed(
+            agent, 
+            message,
+            session = session
+        )
+
+        async for event in stream.stream_events():
+            if event.type == "raw_response_event":
+                if event.data.type == "response.output_text.delta":
+                        response += event.data.delta
+                        text_placeholder.write(response)
+
+############################################
+#################### UI ####################
+############################################
+
 prompt = st.chat_input("Write a message for your assistant")
 
 if prompt:
-    with st.chat_message("human"):
+    with st.chat_message("user"):
         st.write(prompt)
     
     asyncio.run(run_agent(prompt))
